662296e1946da3b0400452990f8177062b0bdfc4
Tsyreniao <wtsofros@gmail.com>
2022-05-22 22:18:59 +0300

    init commit

---
+++ Kursach_3/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Kursach_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AddStudentBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.UpdateDataBtn = new System.Windows.Forms.Button();
+            this.ExportDbAsXmls = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.SelectBtn = new System.Windows.Forms.Button();
+            this.ShowDbBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(653, 335);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseClick);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 377);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 377);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(227, 377);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(333, 377);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Group";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(333, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Email";
+            // 
+            // AddStudentBtn
+            // 
+            this.AddStudentBtn.Location = new System.Drawing.Point(12, 403);
+            this.AddStudentBtn.Name = "AddStudentBtn";
+            this.AddStudentBtn.Size = new System.Drawing.Size(100, 23);
+            this.AddStudentBtn.TabIndex = 10;
+            this.AddStudentBtn.Text = "Add";
+            this.AddStudentBtn.UseVisualStyleBackColor = true;
+            this.AddStudentBtn.Click += new System.EventHandler(this.AddStudentBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(227, 403);
+            this.DeleteBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(100, 23);
+            this.DeleteBtn.TabIndex = 11;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // UpdateDataBtn
+            // 
+            this.UpdateDataBtn.Location = new System.Drawing.Point(120, 403);
+            this.UpdateDataBtn.Name = "UpdateDataBtn";
+            this.UpdateDataBtn.Size = new System.Drawing.Size(100, 23);
+            this.UpdateDataBtn.TabIndex = 12;
+            this.UpdateDataBtn.Text = "Update";
+            this.UpdateDataBtn.UseVisualStyleBackColor = true;
+            this.UpdateDataBtn.Click += new System.EventHandler(this.UpdateDataBtn_Click);
+            // 
+            // ExportDbAsXmls
+            // 
+            this.ExportDbAsXmls.Location = new System.Drawing.Point(439, 347);
+            this.ExportDbAsXmls.Name = "ExportDbAsXmls";
+            this.ExportDbAsXmls.Size = new System.Drawing.Size(98, 23);
+            this.ExportDbAsXmls.TabIndex = 13;
+            this.ExportDbAsXmls.Text = "Export DB.xmls";
+            this.ExportDbAsXmls.UseVisualStyleBackColor = true;
+            this.ExportDbAsXmls.Click += new System.EventHandler(this.ExportDbAsXmls_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(543, 403);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(98, 23);
+            this.ExitBtn.TabIndex = 14;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // SelectBtn
+            // 
+            this.SelectBtn.Location = new System.Drawing.Point(333, 403);
+            this.SelectBtn.Name = "SelectBtn";
+            this.SelectBtn.Size = new System.Drawing.Size(100, 23);
+            this.SelectBtn.TabIndex = 15;
+            this.SelectBtn.Text = "Select";
+            this.SelectBtn.UseVisualStyleBackColor = true;
+            this.SelectBtn.Click += new System.EventHandler(this.SelectBtn_Click);
+            // 
+            // ShowDbBtn
+            // 
+            this.ShowDbBtn.Location = new System.Drawing.Point(439, 375);
+            this.ShowDbBtn.Name = "ShowDbBtn";
+            this.ShowDbBtn.Size = new System.Drawing.Size(98, 23);
+            this.ShowDbBtn.TabIndex = 16;
+            this.ShowDbBtn.Text = "Show DB";
+            this.ShowDbBtn.UseVisualStyleBackColor = true;
+            this.ShowDbBtn.Click += new System.EventHandler(this.ShowDbBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 438);
+            this.Controls.Add(this.ShowDbBtn);
+            this.Controls.Add(this.SelectBtn);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.ExportDbAsXmls);
+            this.Controls.Add(this.UpdateDataBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.AddStudentBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Students Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button AddStudentBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button UpdateDataBtn;
+        private System.Windows.Forms.Button ExportDbAsXmls;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Button SelectBtn;
+        private System.Windows.Forms.Button ShowDbBtn;
+    }
+}
+
---
+++ Kursach_3/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using ClosedXML.Excel;
+
+namespace Kursach_3
+{
+    public partial class Form1 : Form
+    {
+        private int selectedRowId = -1;
+        private bool isSorted = false;
+
+        private SqlConnection sqlConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["StudentsDB"].ConnectionString);
+            sqlConnection.Open();
+
+            if (sqlConnection.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Connection isnt open!");
+            }
+
+            Cleaner();
+        }
+
+        private void Cleaner()
+        {
+            ClearTextBoxes();
+            ShowDB();
+            dataGridView1.ClearSelection();
+            selectedRowId = -1;
+            isSorted = false;
+        }
+
+        private void AddStudentBtn_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 || textBox2.Text.Length > 0 || textBox3.Text.Length > 0 || textBox4.Text.Length > 0)
+            {
+                SqlCommand command = new SqlCommand(
+                "Insert Into [StudentsInfo] ([Name], Surname, [Group], Email) Values (@Name, @Surname, @Group, @Email)",
+                sqlConnection);
+
+                command.Parameters.AddWithValue("Name", textBox1.Text);
+                command.Parameters.AddWithValue("Surname", textBox2.Text);
+                command.Parameters.AddWithValue("Group", textBox3.Text);
+                command.Parameters.AddWithValue("Email", textBox4.Text);
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                Cleaner();
+            }
+            else MessageBox.Show("Enter at least one parameter");
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void ShowDB()
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                "Select * From [StudentsInfo]",
+                sqlConnection);
+
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+
+            dataGridView1.DataSource = dataSet.Tables[0];
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (selectedRowId != -1)
+            {
+                SqlCommand command = new SqlCommand(
+                    "Delete From [StudentsInfo] Where Id=@Id",
+                    sqlConnection);
+
+                command.Parameters.AddWithValue("Id", selectedRowId);
+                command.ExecuteNonQuery();
+
+                Cleaner();
+            }
+            else MessageBox.Show("Select sell or row");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                selectedRowId = Convert.ToInt32(row.Cells["Id"].Value.ToString());
+
+                textBox1.Text = row.Cells["Name"].Value.ToString();
+                textBox2.Text = row.Cells["Surname"].Value.ToString();
+                textBox3.Text = row.Cells["Group"].Value.ToString();
+                textBox4.Text = row.Cells["Email"].Value.ToString();
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                selectedRowId = Convert.ToInt32(row.Cells["Id"].Value.ToString());
+
+                textBox1.Text = row.Cells["Name"].Value.ToString();
+                textBox2.Text = row.Cells["Surname"].Value.ToString();
+                textBox3.Text = row.Cells["Group"].Value.ToString();
+                textBox4.Text = row.Cells["Email"].Value.ToString();
+            }
+        }
+
+        private void UpdateDataBtn_Click(object sender, EventArgs e)
+        {
+            if (selectedRowId != -1)
+            {
+                SqlCommand command = new SqlCommand(
+                    "Update [StudentsInfo] Set Name=@Name, Surname=@Surname, [Group]=@Group, Email=@Email Where Id=@Id",
+                    sqlConnection);
+
+                command.Parameters.AddWithValue("Id", selectedRowId);
+                command.Parameters.AddWithValue("Name", textBox1.Text);
+                command.Parameters.AddWithValue("Surname", textBox2.Text);
+                command.Parameters.AddWithValue("Group", textBox3.Text);
+                command.Parameters.AddWithValue("Email", textBox4.Text);
+                command.ExecuteNonQuery();
+
+                Cleaner();
+            }
+            else MessageBox.Show("Select sell or row");
+        }
+
+        private void ExportDbAsXmls_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx" };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (XLWorkbook workbook = new XLWorkbook())
+                    {
+                        if (isSorted == false)
+                        {
+                            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                            "Select * From [StudentsInfo]",
+                            sqlConnection);
+
+                            DataTable dataTable = new DataTable();
+                            sqlDataAdapter.Fill(dataTable);
+
+                            workbook.Worksheets.Add(dataTable, "StudentsInfo");
+                            workbook.SaveAs(sfd.FileName);
+                        }
+                        else
+                        {
+                            string parameters = TextboxesString();
+
+                            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                            $"Select * From [StudentsInfo] Where {parameters}",
+                            sqlConnection);
+
+                            DataTable dataTable = new DataTable();
+                            sqlDataAdapter.Fill(dataTable);
+
+                            workbook.Worksheets.Add(dataTable, "StudentsInfo");
+                            workbook.SaveAs(sfd.FileName);
+                        }
+                    }
+                    MessageBox.Show("DataBase is successful exported as excel file");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void SelectBtn_Click(object sender, EventArgs e)
+        {
+            string parameters = TextboxesString();
+
+            if (parameters.Length > 1)
+            {
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                $"Select * From [StudentsInfo] Where {parameters}",
+                sqlConnection);
+
+                DataSet dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet);
+
+                dataGridView1.DataSource = dataSet.Tables[0];
+                dataGridView1.AllowUserToAddRows = false;
+
+                isSorted = true;
+            }
+            else
+            {
+                MessageBox.Show("Enter parameters");
+            }
+            dataGridView1.ClearSelection();
+            selectedRowId = -1;
+        }
+
+        private string TextboxesString()
+        {
+            string parameters = "";
+            string parametersMinus1 = "";
+
+            if (textBox1.Text != "")
+            {
+                parameters += "Name Like ";
+                parameters += "N'%";
+                parameters += textBox1.Text;
+                parameters += "%'";
+                parameters += "And ";
+            }
+            if (textBox2.Text != "")
+            {
+                parameters += "Surname Like ";
+                parameters += "N'%";
+                parameters += textBox2.Text;
+                parameters += "%'";
+                parameters += "And ";
+            }
+            if (textBox3.Text != "")
+            {
+                parameters += "[Group] Like ";
+                parameters += "N'%";
+                parameters += textBox3.Text;
+                parameters += "%'";
+                parameters += "And ";
+            }
+            if (textBox4.Text != "")
+            {
+                parameters += "Email Like ";
+                parameters += "N'%";
+                parameters += textBox4.Text;
+                parameters += "%'";
+                parameters += "And ";
+            }
+            if (parameters.Length > 0)
+            {
+                parametersMinus1 = parameters.Remove(parameters.Length - 4, 4);
+            }
+            return parametersMinus1;
+        }
+
+        private void ShowDbBtn_Click(object sender, EventArgs e)
+        {
+            Cleaner();
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dataGridView1.ClearSelection();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
